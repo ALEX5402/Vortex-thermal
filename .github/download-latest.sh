94732270f07e83a67bbdcb4c987c71edd596ab83
Alex <alex5402private@gmail.com>
2024-07-27 19:29:55 +0530

    upgraded to 1.0.2

--- .github/download-latest.sh
+++ .github/download-latest.sh
@@ -4,24 +4,18 @@
 REPO="alex5402/Vortex-thermal"
 
 # Fetch the latest release information from the GitHub API using curl
-RELEASE_INFO=$(curl -L -H "Accept: application/vnd.github+json" "https://api.github.com/repos/$REPO/releases/latest")
+RELEASE_INFO=$(curl -L -H "Accept: application/vnd.github+json" "https://api.github.com/repos/alex5402/Vortex-thermal/releases/latest")
 
 # Extract the tarball URL
-TARBALL_URL=$(echo "$RELEASE_INFO" | jq -r '.tarball_url')
+TAGNAME=$(echo "$RELEASE_INFO" | jq -r '.tag_name')
 
-
-# Check if TARBALL_URL is empty
-if [ -z "$TARBALL_URL" ]; then
-    echo "No tarball URL found in the latest release."
-    exit 1
-fi
 
 # Define the output file name
 OUTPUT_FILE="latest-release.tar.gz"
 
 # Use wget to download the tarball
 echo "Downloading from $TARBALL_URL"
-wget -O "$OUTPUT_FILE" "$TARBALL_URL"
+wget -O "$OUTPUT_FILE" "https://github.com/ALEX5402/Vortex-thermal/archive/refs/tags/$TAGNAME.tar.gz"
 
 # Confirm completion
 echo "Downloaded the tarball and saved it as $OUTPUT_FILE."
